69dd924d6f57e0a6e000e40074b1e2dbdb237080
Alexander Kolb <alexander.kolb@ottogroup.com>
2015-12-18 15:11:10 +0100

    new try

--- flinkspector-datastream-scala/src/test/scala/org/flinkspector/scala/datastream/DataStreamTest.scala
+++ flinkspector-datastream-scala/src/test/scala/org/flinkspector/scala/datastream/DataStreamTest.scala
@@ -17,7 +17,6 @@ package org.flinkspector.scala.datastream
  */
 
 
-
 import org.apache.flink.api.scala._
 import org.flinkspector.core.collection.ExpectedRecords
 import org.flinkspector.core.input.InputBuilder
@@ -27,13 +26,13 @@ import org.scalatest.Matchers
 import org.scalatest.matchers.Matcher
 
 
-class ProductMatcher[T <: Product,V](key: String,
-                                   matcher: Matcher[V],
-                                   mask: List[String])
+class ProductMatcher[T <: Product, V](key: String,
+                                      matcher: Matcher[V],
+                                      mask: List[String])
   extends TypeSafeDiagnosingMatcher[T] {
 
   def resolve(key: String, product: Product): V = {
-    product.productElement(mask.indexOf(key))
+    product.productElement(mask.indexOf(key)).asInstanceOf[V]
   }
 
   override def matchesSafely(item: T, mismatchDescription: Description): Boolean = {
@@ -57,14 +56,16 @@ class ProductMatcher[T <: Product,V](key: String,
 class MatchProduct[T <: Product](mask: List[String])
   extends MatchRecords[T] with Matchers {
 
+  case class Test3()
+
   def assertThat[V](key: String, matcher: Matcher[V]): MatchProduct[T] = {
-    super.assertThat(new ProductMatcher[T,V](key, matcher, mask))
+    super.assertThat(new ProductMatcher[T, V](key, matcher, mask))
     this
   }
 
 }
 
-
+case class Test(s: String)
 
 class DataStreamTest extends CoreSpec {
 
@@ -84,6 +85,30 @@ class DataStreamTest extends CoreSpec {
 
   "assert test" should "do shi" in {
 
+    import scala.reflect.runtime.{universe => u}
+
+    def companionMembers(clazzTag: scala.reflect.ClassTag[_]): u.MemberScope = {
+      val runtimeClass = clazzTag.runtimeClass
+      val rootMirror = u.runtimeMirror(runtimeClass.getClassLoader)
+      val classSymbol = rootMirror.classSymbol(runtimeClass)
+      // get the companion here
+      println(classSymbol.typeSignature.members)
+
+      println(classSymbol.companion)
+
+      classSymbol.companion.typeSignature.members
+    }
+
+    case class MyClass(value: Int)
+
+    println(companionMembers(scala.reflect.classTag[MyClass]))
+
+    val applyMethods =
+      companionMembers(scala.reflect.classTag[MyClass])
+        .filter { m => m.isMethod && m.name.toString == "apply"}
+
+    println(applyMethods)
+
     val env = DataStreamTestEnvironment.createTestEnvironment(1)
     val input = InputBuilder.startWith(("test", 9))
       .emit(("check", 5))
@@ -91,17 +116,18 @@ class DataStreamTest extends CoreSpec {
       .emit(("check", 11))
       .emit(("check", 23))
 
-    val expected = new MatchProduct[(String,Int)](List("key","value"))
-    .assertThat("key", be(10))
-    .assertThat("key", be > 10)
+    val expected = new MatchProduct[(String, Int)](List("key", "value"))
+      .assertThat("key", be(10))
+      .assertThat("key", be > 10)
 
     case class Foo(key: String, value: Int)
+    case class Bar(key: String)
 
-    val tuple = "test"
+    val tuple = ("test",1)
 
-    def convert
 
-    Foo.getClass.getMethods.find(x => x.getName == "apply" && x.isBridge).get.invoke(Foo, params map (_.asInstanceOf[AnyRef]): _*).asInstanceOf[Foo]
+
+
 
   }
 
